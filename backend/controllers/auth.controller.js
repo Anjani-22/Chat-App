a984075e49eb6ceebc5f929cef5f2dbc2b4f6e7a
Anjani-22 <anjaniofficial22@gmail.com>
2024-03-01 00:02:16 +0530

    logout, login

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import chatUser from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import generateJWTToken from "../utils/generateJWTToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -12,14 +14,16 @@ export const signup = async (req, res) => {
       return res.status(400).json({ error: "passoword dont match" });
     }
 
-    const chatuser = await chatUser.find({ username: username });
-    console.log("funduser ", chatuser);
+    const chatuser = await chatUser.findOne({ username: username });
 
-    if (chatuser && chatuser.length !== 0) {
+    if (chatuser) {
       return res.status(400).json({ error: "Username already exists" });
     }
     //Hash password
     //https://avatar-placeholder.iran.liara.run/
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
 
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
     const girlProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
@@ -27,27 +31,65 @@ export const signup = async (req, res) => {
     const newchatUser = new chatUser({
       fullname,
       username,
-      password,
+      password: hashPassword,
       gender,
       profilePic: gender === "female" ? girlProfilePic : boyProfilePic,
     });
 
-    await newchatUser.save();
+    if (newchatUser) {
+      generateJWTToken(newchatUser._id, res);
+      await newchatUser.save();
 
-    res.status(201).json({
-      _id: newchatUser.id,
-      fullname: newchatUser.fullname,
-      username: newchatUser.username,
-      profilePic: newchatUser.profilePic,
-    });
+      //jwt
+
+      res.status(201).json({
+        _id: newchatUser.id,
+        fullname: newchatUser.fullname,
+        username: newchatUser.username,
+        profilePic: newchatUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "invalid user data" });
+    }
   } catch (error) {
-    console.log("😀Error", error.message, error);
+    console.log("😀Error Signup", error.message, error);
     res.status(500).json({ error: "server side error" });
   }
 };
-export const login = (req, res) => {
-  res.send("Login");
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await chatUser.findOne({ username });
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      res.status(400).json({ error: "invalid user or password" });
+    }
+
+    generateJWTToken(user._id, res);
+
+    res.status(201).json({
+      _id: user.id,
+      fullname: user.fullname,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("😀Error Login", error.message, error);
+    res.status(500).json({ error: "server side error" });
+  }
 };
-export const logout = (req, res) => {
-  res.send("Logout");
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logout succesfully" });
+  } catch (error) {
+    console.log("😀Error Logout", error.message, error);
+    res.status(500).json({ error: "server side error" });
+  }
 };
