b9e4914d2058a286d5d5661d9fa6f887af3bd84d
Anjani-22 <anjaniofficial22@gmail.com>
2024-03-02 20:11:13 +0530

    backend completed except socket.io

--- backend/controllers/getUsersForSideBar.controller.js
+++ backend/controllers/getUsersForSideBar.controller.js
@@ -1,6 +1,16 @@
+import chatUser from "../models/chatUser.model.js";
+
 export const getUsersForSideBar = async (req, res) => {
   try {
     const loggedInUserId = req.chatUser._id;
+
+    const filteredChatUsers = await chatUser
+      .find({
+        _id: { $ne: loggedInUserId },
+      })
+      .select("-password");
+
+    res.status(200).json(filteredChatUsers);
   } catch (error) {
     console.log("getUsersForSidebar", error);
     res.status(500).json({ error: "Internal Server Error" });
